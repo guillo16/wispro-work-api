b11e1dbac4bcb901031c75044d640b2566ed213a
guillermo varela <guillovarela16@gmail.com>
2021-08-09 08:50:21 -0300

    Finish model testing

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,33 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @post = Post.new(title: "Ruby developer",
+      content: "wonderfull opportunity",
+      user: users(:company)
+    )
+  end
+
+  test "should be valid" do
+    assert @post.valid?, "Should be valid if all required fields are set"
+  end
+
+  test "should be invalid if title is nil" do
+    @post.title = nil
+
+    assert_not @post.valid?
+  end
+
+  test "should be invalid if content is nil" do
+    @post.content = nil
+
+    assert_not @post.valid?
+  end
+
+  test "should be invalid if user is nil" do
+    @post.user = nil
+
+    assert_not @post.valid?
+  end
+
 end
--- test/models/postulation_test.rb
+++ test/models/postulation_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class PostulationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+
+    @postulation = Postulation.new(
+      description: "The best candidate ever",
+      user: users(:person),
+      post: posts(:first_post)
+    )
+  end
+
+  test "should be valid if all fields are set" do
+    assert @postulation.valid?, "Should be valid if all required fields are set"
+  end
+
+  test "should be invalid if description is nil" do
+    @postulation.description = nil
+
+    assert_not @postulation.valid?
+  end
+
+  test "should be invalid if user is nil" do
+    @postulation.user = nil
+
+    assert_not @postulation.valid?
+  end
+
+  test "should be invalid if post is nil" do
+    @postulation.post = nil
+
+    assert_not @postulation.valid?
+  end
+
 end
