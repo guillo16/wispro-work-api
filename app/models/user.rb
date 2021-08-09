770f7af885a870d83a1b0445fe34dccc28f807b4
guillermo varela <guillovarela16@gmail.com>
2021-08-08 21:18:52 -0300

    Add jwt gem

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  enum role_level: { person: 0, company: 1 }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
