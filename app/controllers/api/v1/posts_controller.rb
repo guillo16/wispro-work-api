4a71fa2abea0d83fc091cbc40a94e55bf0822505
guillermo varela <guillovarela16@gmail.com>
2021-08-08 22:40:45 -0300

    Finish posts policies and controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,14 +1,43 @@
 class Api::V1::PostsController < Api::V1::BaseController
+  before_action :set_posts, only: %w[show destroy]
+
   def index
-    @posts = Post.all
+    @posts = policy_scope(Post)
   end
 
   def show
+    @post = Post.find(params[:id])
+    authorize @post
   end
 
   def create
+    @post = post.new(post_params)
+    @post.user = current_user
+    authorize @post
+    if @post.save
+      render :show, status: :created
+    else
+      render_error
+    end
   end
 
   def destroy
+    @post.destroy
+    authorize @post
+    render :index, status: :destroyed
+  end
+
+  private
+
+  def set_posts
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title)
+  end
+
+  def render_error
+    render json: { errors: @post.errors.full_messages }, status: :unprocessable_entity
   end
 end
--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -4,4 +4,16 @@ class PostPolicy < ApplicationPolicy
       scope.all
     end
   end
+
+  def show?
+    true
+  end
+
+  def create?
+    user.company?
+  end
+
+  def destroy?
+    record.user == user
+  end
 end
