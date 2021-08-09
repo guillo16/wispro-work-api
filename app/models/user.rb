16691b0c322b5db6413e1e4d2323e24b05424bfa
guillermo varela <guillovarela16@gmail.com>
2021-08-08 22:09:30 -0300

    Finish postulation models

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
          :registerable,
          jwt_revocation_strategy: JwtDenylist
 
-  has_many :posts
+  has_many :posts, dependent: :destroy
+  has_many :postulations, dependent: :destroy
 
   validates :email, presence: true
   validates :email, uniqueness: true
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_010203) do
+ActiveRecord::Schema.define(version: 2021_08_09_010759) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,16 @@ ActiveRecord::Schema.define(version: 2021_08_09_010203) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "postulations", force: :cascade do |t|
+    t.string "description"
+    t.bigint "user_id", null: false
+    t.bigint "post_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["post_id"], name: "index_postulations_on_post_id"
+    t.index ["user_id"], name: "index_postulations_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -44,4 +54,6 @@ ActiveRecord::Schema.define(version: 2021_08_09_010203) do
   end
 
   add_foreign_key "posts", "users"
+  add_foreign_key "postulations", "posts"
+  add_foreign_key "postulations", "users"
 end
